fdb5f0bf68e58fe239bcc43ab995598b98341783
Patkarn0503 <patthakarn0503@gmail.com>
2026-06-10 00:04:15 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'win-auto-v2';
+var CACHE = 'win-auto-v3';
 var FILES = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', function(e){
@@ -11,7 +11,10 @@ self.addEventListener('install', function(e){
 self.addEventListener('activate', function(e){
   e.waitUntil(
     caches.keys().then(function(keys){
-      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; })
+            .map(function(k){ return caches.delete(k); })
+      );
     }).then(function(){ return self.clients.claim(); })
   );
 });
